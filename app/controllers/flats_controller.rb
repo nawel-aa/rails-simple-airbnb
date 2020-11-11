2e1c3b5bb2814e4b16d4025735a4b1dc225bb207
Nawel Aammar <nawel.aa@hotmail.fr>
2020-11-11 16:52:13 -0500

    Working on getting a map for the show page

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,4 +1,9 @@
+require 'open-uri'
+require 'json'
+
 class FlatsController < ApplicationController
+  before_action :find_flat, only: (:show)
+
   def index
     @flats = Flat.all
   end
@@ -17,9 +22,20 @@ class FlatsController < ApplicationController
     end
   end
 
+  def show
+    address = CGI.escape(@flat.address)
+    coordinates_url = "http://api.mapbox.com/geocoding/v5/mapbox.places/#{address}.json?access_token=pk.eyJ1IjoibmF3ZWxpd2VsbyIsImEiOiJja2d3aXM4MTAwYW13MnhvM3pianE3dDBrIn0.9VUFokyb6AlwRvQWtID49Q"
+    coordinates = JSON.parse(open(coordinates_url).read)
+
+  end
+
   private
 
   def flat_params
     params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
+  def find_flat
+    @flat = Flat.find(params[:id])
   end
 end
